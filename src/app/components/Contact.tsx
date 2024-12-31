abfa5ce41b5fa7cf38638c551d6066fe83916291
Naveen <naveenlakmal.edu@gmail.com>
2024-12-31 16:14:42 +0530

    current changing hero page

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -3,9 +3,10 @@ import Heading from './Heading'
 
 const Contact = () => {
     return (
-        <section className='container mx-auto py-10 px-4'>
+        <section className='container mx-auto py-10 px-4 '>
             <Heading  title='Contact Us' />
-            <form className='flex flex-col gap-8 lg:px-32'>
+            <form className='flex flex-col gap-8 lg:px-32 '>
+                {/* <div className='border border-rose-700 px bg-green-500 border-2 border-gray-800 rounded-lg p-6 text-white blur-0'> */}
                 <div className="grid grid-cols-1 sm:grid-cols-2  gap-8">
                     <input type="text" placeholder='Name' className='contactUsInput bg-[#414657]' required />
                     <input type="email" placeholder='email' className='contactUsInput bg-[#414657]' required />
@@ -18,6 +19,7 @@ const Contact = () => {
 
                 <div>
                     <button type='submit' className='btn '>Send Message</button>
+                {/* </div> */}
                 </div>
             </form>
 
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -6,21 +6,27 @@ import Button from './Button';
 
 const Hero = () => {
     return (
-        <div className=' h-screen bg-cover bg-center bg-fixed flex items-center '>
-            <Image src="/baground.jpg" alt="Background" fill className="object-cover z-[-1]" priority />
-            <div className="container mx-auto px-4 ">
-                <div className="max-w-[450px] text-white flex flex-col gap-[40px] " >
-                    <div>
-                        <h1 className='text-5xl '>I'm Naveen</h1>
-                        <h4 className='text-2xl mt-3'></h4>
-                       Fullstack Developer
+        <div className=' h-screen bg-cover bg-center bg-fixed flex items-center bg-[#181c30] border border-gray-300'>
+            {/* <Image src="/baground.jpg" alt="Background" fill className="object-cover z-[-1]" priority /> */}
+            <div className='flex flex-col sm:flex-row gap-10'>
+                <div className="container mx-auto px-4 border border-gray-300">
+                    <div className="max-w-[450px] text-white flex flex-col gap-[40px] " >
+                        <div>
+                            <h1 className='text-5xl '>I'm Naveen</h1>
+                            <h4 className='text-2xl mt-3'></h4>
+                            Fullstack Developer
 
+                        </div>
+                        <div className='bg-[#ffffff] h-[2px] w-[40px]'></div>
+                        <div>
+                            <p>In a Professional context it often happens that private clients corder a publication  to be made.</p>
+                            <Button link="#" text="learn More"></Button>
+                        </div>
                     </div>
-                    <div className='bg-[#ffffff] h-[2px] w-[40px]'></div>
-                    <div>
-                        <p>In a Professional context it often happens that private clients corder a publication  to be made.</p>
-                        <Button link="#" text="learn More"></Button>
-                    </div>
+                </div>
+
+                <div className='w-96 h-96 rounded-full bg-blue-500'>
+                    {/* <img className="w-[340px] h-[360px]   border border-green-800" src="/about.jpg" alt="about" /> */}
                 </div>
             </div>
         </div>
