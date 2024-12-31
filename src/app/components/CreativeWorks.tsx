a88f833610276742bbea748729e32e3dfe47cf8e
Naveen <naveenlakmal.edu@gmail.com>
2024-12-31 11:43:15 +0530

    change gallery responsive setting

--- src/app/components/CreativeWorks.tsx
+++ src/app/components/CreativeWorks.tsx
@@ -5,7 +5,7 @@ const CreativeWorks = () => {
   return (
     <section className='container mx-auto py-10 px-4'>
         <Heading title="Creative Works"></Heading>
-        <div className="grid grid-cols-3 gap-4 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:px-16 px-6">
             <div className="row-span-2 relative group">
                 <img src="/sample01.jpg" alt="" />
                 <div className="bg-[#000000bd] absolute w-[100%] h-[100%] top-0 opacity-0 transition duration-500 group-hover:opacity-100 grid place-items-center text-white">Project title</div>
