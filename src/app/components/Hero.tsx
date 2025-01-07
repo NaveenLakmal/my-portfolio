9670dbd651ae94ec0d881fd5593ae800545dbd2f
Naveen <naveenlakmal.edu@gmail.com>
2025-01-07 16:11:22 +0530

    make responsive in Hero page

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 
 import Button from './Button';
@@ -6,28 +7,38 @@ import Button from './Button';
 
 const Hero = () => {
     return (
-        <div className=' h-screen bg-cover bg-center bg-fixed flex items-center bg-[#181c30] border border-gray-300'>
+        <div className=' h-screen bg-cover bg-center bg-fixed flex items-center bg-[#181c30] '>
             {/* <Image src="/baground.jpg" alt="Background" fill className="object-cover z-[-1]" priority /> */}
-            <div className='flex flex-col sm:flex-row gap-10'>
-                <div className="container mx-auto px-4 border border-gray-300">
-                    <div className="max-w-[450px] text-white flex flex-col gap-[40px] " >
+
+            <div className='flex flex-col sm:flex-row sm:gap-8 border border-lime-700 mt-40 sm:mt-0   '>
+                <div className="container mx-auto px-4 border border-gray-300 my-8 sm:my-0">
+                    <div className="max-w-[450px] text-white flex flex-col gap-[40px] border border-amber-500" >
                         <div>
-                            <h1 className='text-5xl '>I&apos;m Naveen</h1>
-                            <h4 className='text-2xl mt-3'></h4>
-                            Fullstack Developer
+                            <h1 className='text-5xl mt-4'>I&apos;m Naveen</h1>
+                            <h4 className='text-2xl mt-2'>Fullstack Developer</h4>
+                            <div className='bg-[#ffffff] h-[2px] w-[40px] mt-5'></div>
+
 
                         </div>
-                        <div className='bg-[#ffffff] h-[2px] w-[40px]'></div>
+
                         <div>
-                            <p>In a Professional context it often happens that private clients corder a publication  to be made.</p>
+                            <p>In a Professional context it often happens that private clients corder a
+                                publication  to be made.</p>
                             <Button link="#" text="learn More"></Button>
                         </div>
                     </div>
                 </div>
 
-                {/* <div className='w-96 h-96 rounded-full bg-blue-500'> */}
-                    {/* <img className="w-[340px] h-[360px]   border border-green-800" src="/about.jpg" alt="about" /> */}
-                {/* </div> */}
+                <div className="ml-14 sm:ml-0 ">   
+                    <Image
+                        className="w-[240px] h-[240px] sm:w-[680px] sm:h-[420px] border border-green-800 rounded-full object-cover"
+                        src="/about.jpg"
+                        width={500}
+                        height={500}
+                        alt="about"
+                    />
+                </div>
+
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    
     extend: {},
   },
   plugins: [],
